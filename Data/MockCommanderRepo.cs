9da0f385398373d9b744241831d316d49f5608af
absandell <absandell@gmail.co>
2024-01-28 17:14:24 -0500

    Working basic endpoints

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Foobar1", Line="FizzBuzz1", Platform="Test1" },
+                new Command{Id=1, HowTo="Foobar2", Line="FizzBuzz2", Platform="Test2" },
+                new Command{Id=2, HowTo="Foobar3", Line="FizzBuzz3", Platform="Test3" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Foobar", Line="FizzBuzz", Platform="Test" };
+        }
+    }
+}
